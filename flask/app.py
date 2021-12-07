e3801a2fe9f9cf34511c9b31bba8339f4790e99f
Ricardo Tejerina <ricardo.tejerina314@gmail.com>
2021-12-07 07:55:32 -0400

    backend actualized

--- flask/app.py
+++ flask/app.py
@@ -25,20 +25,25 @@ class Usuario(db.Model):
         self.rol = rol
 
 class Empresa(db.Model):
-    id = db.Column(db.String(255), primary_key=True)
+    id = db.Column(db.Integer, primary_key=True)
     nombre = db.Column(db.String(255))
-    indice_jerarquico = db.Column(db.Integer)
     fecha_creacion = db.Column(db.String(255))
     fecha_cierre = db.Column(db.String(255))
     descripcion = db.Column(db.String(255))
     funciones = db.Column(db.String(255))
     estado = db.Column(db.String(255))
-    empresa_superior = db.Column(db.String(255), ForeignKey('empresa.id'))
-    empresa = relationship("Empresa")
+
+    def __init__(self, nombre, fecha_creacion, fecha_cierre, descripcion, funciones, estado):
+        self.nombre = nombre
+        self.fecha_creacion = fecha_creacion
+        self.fecha_cierre = fecha_cierre
+        self.descripcion = descripcion
+        self.funciones = funciones
+        self.estado = estado
 
 class Edificio(db.Model):
-    id = db.Column(db.String(255), primary_key=True)
-    id_empresa = db.Column(db.String(255), ForeignKey('empresa.id'))
+    id = db.Column(db.Integer, primary_key=True)
+    id_empresa = db.Column(db.Integer, ForeignKey('empresa.id'))
     empresa = relationship("Empresa")
     coordenadas = db.Column(db.String(255))
     nombre = db.Column(db.Integer)
@@ -48,9 +53,19 @@ class Edificio(db.Model):
     estado = db.Column(db.String(255))
     cantidad_personas = db.Column(db.Integer) 
 
+    def __init__(self, id_empresa, coordenadas, nombre, tipo, direccion, distrito, estado, cantidad_personas):
+        self.nombre = nombre
+        self.id_empresa = id_empresa
+        self.coordenadas = coordenadas
+        self.tipo = tipo
+        self.direccion = direccion
+        self.distrito = distrito
+        self.estado = estado
+        self.cantidad_personas = cantidad_personas
+
 class Escala(db.Model):
-    id = db.Column(db.String(255), primary_key=True)
-    id_empresa = db.Column(db.String(255), ForeignKey('empresa.id'))
+    id = db.Column(db.Integer, primary_key=True)
+    id_empresa = db.Column(db.Integer, ForeignKey('empresa.id'))
     empresa = relationship("Empresa")
     categoria = db.Column(db.String(255))
     sub_categoria = db.Column(db.Integer)
@@ -66,23 +81,53 @@ class Escala(db.Model):
     fecha_aprobacion = db.Column(db.String(255))
     fecha_cierre = db.Column(db.String(255))
 
+    def __init__(self, id_empresa, categoria, sub_categoria, clase, descripcion, nivel_salarial,
+                nombre_puesto, haber_basico, numero_items, costo_mensual, costo_anual, fecha_aprobacion, fecha_cierre):
+        self.id_empresa = id_empresa
+        self.categoria = categoria
+        self.sub_categoria = sub_categoria
+        self.clase = clase
+        self.descripcion = descripcion
+        self.nivel_salarial = nivel_salarial
+        self.nombre_puesto = nombre_puesto
+        self.haber_basico = haber_basico
+        self.numero_items = numero_items
+        self.costo_anual = costo_anual
+        self.costo_mensual = costo_mensual
+        self.fecha_aprobacion = fecha_aprobacion
+        self.fecha_cierre = fecha_cierre
+
 class CambiosEm(db.Model):
-    id = db.Column(db.String(255), primary_key=True)
-    id_empresa = db.Column(db.String(255), ForeignKey('empresa.id'))
+    id = db.Column(db.Integer, primary_key=True)
+    id_empresa = db.Column(db.Integer, ForeignKey('empresa.id'))
     empresa = relationship("Empresa")
     operacion = db.Column(db.String(255))
     fecha = db.Column(db.Integer)
     motivo = db.Column(db.String(255))
     documentacion = db.Column(db.String(255))
 
+    def __init__(self, id_empresa, operacion, fecha, motivo, documentacion):
+        self.id_empresa = id_empresa
+        self.operacion = operacion
+        self.fecha = fecha
+        self.motivo = motivo
+        self.documentacion = documentacion
+
 class CambiosEd(db.Model):
-    id = db.Column(db.String(255), primary_key=True)
-    id_edificio = db.Column(db.String(255), ForeignKey('edificio.id'))
-    empresa = relationship("Empresa")
+    id = db.Column(db.Integer, primary_key=True)
+    id_edificio = db.Column(db.Integer, ForeignKey('edificio.id'))
+    edificio = relationship("Edificio")
     operacion = db.Column(db.String(255))
     fecha = db.Column(db.Integer)
     motivo = db.Column(db.String(255))
     documentacion = db.Column(db.String(255))
+
+    def __init__(self, id_edificio, operacion, fecha, motivo, documentacion):
+        self.id_edificio = id_edificio
+        self.operacion = operacion
+        self.fecha = fecha
+        self.motivo = motivo
+        self.documentacion = documentacion
 
 
 # Creates the database
@@ -98,8 +143,8 @@ users_schema = UsuarioSchema(many=True)
 
 class EmpresaSchema(ma.Schema):
     class Meta:
-        fields = ('id', 'nombre', 'indice_jerarquico', 'fecha_creacion',
-        'fecha_cierre', 'descripcion', 'funciones', 'estado', 'empresa_superior')
+        fields = ('id', 'nombre', 'fecha_creacion',
+        'fecha_cierre', 'descripcion', 'funciones', 'estado')
 
 empresa_schema = EmpresaSchema()
 empresas_schema = EmpresaSchema(many=True)
@@ -122,19 +167,56 @@ class EscalaSchema(ma.Schema):
 escala_schema = EmpresaSchema()
 escalas_schema = EmpresaSchema(many=True)
 
-@app.route('/users', methods=['Post'])
-def create_user():
-    nombre = request.json['nombre']
-    apellido = request.json['apellido']
-    rol = request.json['rol']
+class CambioEd(ma.Schema):
+    class Meta:
+        fields = ('id', 'id_edificio', 'operacion', 'fecha', 'motivo', 'documentacion')
 
-    new_user = Usuario(nombre, apellido, rol)
+cambioEd_schema = EmpresaSchema()
+cambioEds_schema = EmpresaSchema(many=True)
+
+class CambioEm(ma.Schema):
+    class Meta:
+        fields = ('id', 'id_empresa', 'operacion', 'fecha', 'motivo', 'documentacion')
+
+cambioEm_schema = EmpresaSchema()
+cambioEms_schema = EmpresaSchema(many=True)
+
+# Operations
+
+@app.route('/empresas/crear', methods=['Post'])
+def create_empresa():
+    nombre = request.json['nombre']
+    fecha_creacion = request.json['fecha_creacion']
+    fecha_cierre = request.json['fecha_cierre']
+    descripcion = request.json['descripcion']
+    funciones = request.json['funciones']
+    estado = request.json['estado']
+
+    new_empresa = Empresa(nombre, fecha_creacion,
+    fecha_cierre, descripcion, funciones, estado)
     
-    db.session.add(new_user)
+    db.session.add(new_empresa)
     db.session.commit()
 
-    return user_schema.jsonify(new_user)
+    return user_schema.jsonify(new_empresa)
 
+@app.route('/empresas', methods=['Get'])
+def mostrar_empresas():
+    all_empresas = Empresa.query.all()
+    result = empresas_schema.dump(all_empresas)
+    return jsonify(result)
+
+@app.route('/empresas/<id>', methods=['Get'])
+def mostrar_empresa(id):
+    empresa = Empresa.query.get(id)
+    return empresa_schema.jsonify(empresa)
+
+@app.route('/empresas/<id>', methods=['DELETE'])
+def delete_empresa(id):
+  empresa = Empresa.query.get(id)
+  db.session.delete(empresa)
+  db.session.commit()
+  return empresa_schema.jsonify(empresa)
 
 if __name__ == "__main__":
     app.run(debug=True)
