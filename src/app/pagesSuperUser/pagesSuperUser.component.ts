3b47cebab9ac69fd7aa1f99904eb8be188ac6b31
AleChirinos <alejandra.chirinos2702@gmail.com>
2021-12-20 16:12:50 -0400

    Logout para los usuarios

--- src/app/pagesSuperUser/pagesSuperUser.component.ts
+++ src/app/pagesSuperUser/pagesSuperUser.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-pagesSuperUser',
@@ -7,9 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PagesSuperUserComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router :Router) { }
 
   ngOnInit() {
+  }
+
+  gotToLogin() : void{
+    this.router.navigate(['login']);
   }
 
 }
